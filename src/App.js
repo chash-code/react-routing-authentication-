49b984196c93a2668ba81e266d2417ad6a3f732e
chash-code <panchalchash@gmail.com>
2025-12-25 09:14:36 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Todos from './components/Todos';
+import TodoDetails from './components/TodoDetails';
+import ProtectedRoute from './components/ProtectedRoute';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            {/* Unprotected Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            
+            {/* Protected Routes */}
+            <Route 
+              path="/todos" 
+              element={
+                <ProtectedRoute>
+                  <Todos />
+                </ProtectedRoute>
+              } 
+            />
+            
+            <Route 
+              path="/todos/:todoId" 
+              element={
+                <ProtectedRoute>
+                  <TodoDetails />
+                </ProtectedRoute>
+              } 
+            />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
